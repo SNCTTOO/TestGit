42beac9ce7f872ec06fca5abdafc40a0cead5b5a
SNCT <540264573@qq.com>
2017-07-20 14:51:58 +0800

    写了正确的注释

--- app/src/main/java/com/m520it/testgit/MainActivity.java
+++ app/src/main/java/com/m520it/testgit/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 		//BÐ´ÁË¸ö×¢ÊÍ
+		//
         setContentView(R.layout.activity_main);
     }
 }
